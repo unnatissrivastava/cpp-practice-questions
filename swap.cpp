15899b77e2d1e19387d85a51100dc321dfa8eedf
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-13 10:07:25 +0530

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, temp, b;
+    cout<< "enter a number";
+    cin>> a;
+    cout <<"enter another number";
+    cin>> b;
+    temp = a;
+    a = b;
+    b = temp;
+    cout<<"numbers are: "<<a<<b;
+    return 0;
+}
