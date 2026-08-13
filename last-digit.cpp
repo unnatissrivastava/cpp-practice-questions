8fd52fa6f76986e30f2942a19495e12e04ffbb18
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-13 10:06:27 +0530

    Update last-digit.cpp

--- last-digit.cpp
+++ last-digit.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, last;
+    cout<< "enter a number";
+    cin>> a;
+    last = a%10;
+    cout<<"last digit is: "<<last;
 
+    return 0;
+}
