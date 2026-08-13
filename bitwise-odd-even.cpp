edcc396f1ef6ef0ff447aab1794510488a5c69f0
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-13 10:54:29 +0530

    Update bitwise-odd-even.cpp

--- bitwise-odd-even.cpp
+++ bitwise-odd-even.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a;
+    cout<<"enter a number: ";
+    cin >>a;
+    if (a & 1)
+        cout << "odd";
+    else
+        cout << "even";
+    return 0;
+}
