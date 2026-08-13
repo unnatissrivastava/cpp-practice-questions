5d679ce0b8026c0a0640ad9593eb9058d7377b8b
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-13 10:16:31 +0530

    Update swap-without-temp.cpp

--- swap-without-temp.cpp
+++ swap-without-temp.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+     int a,b;
+     cout<<"enter a number: ";
+     cin >> a;
+     cout <<"enter another number: ";
+     cin >> b;
+     cout <<"a:"<<b;
+     cout<< "b:"<<a;
+    return 0;
+}
