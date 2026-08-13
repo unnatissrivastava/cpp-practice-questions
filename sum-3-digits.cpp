bc5b7479f8008a848429ad0fd48dc9ac7e47da48
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-13 10:40:48 +0530

    Update sum-3-digits.cpp

--- sum-3-digits.cpp
+++ sum-3-digits.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a;
+    cout << "Enter a 3-digit number: ";
+    cin >> a;
+
+    int f  = a / 100;          
+    int s = (a / 10) % 10;    
+    int t  = a % 10;           
+    int sum = f + s + t;
+
+    cout << "Sum of digits: " << sum << endl;
+    return 0;
+}
