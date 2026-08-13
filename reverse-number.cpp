49a01405c7a5cbecc51946656bf9f8a6db77913e
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-13 11:10:23 +0530

    Update reverse-number.cpp

--- reverse-number.cpp
+++ reverse-number.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a, digit, reverse = 0, number;
+
+    cout << "enter a number";
+    cin >> a;
+
+    number = a;
+
+    while (number > 0) {
+        digit = number % 10;
+        reverse = reverse * 10 + digit;
+        number = number / 10;
+    }
+
+    cout << "Reverse: " << reverse;
+
+    return 0;
+}
