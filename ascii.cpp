d3d7849f7faa579f9928878681fe668867e7723c
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-24 21:41:33 +0530

    Update ascii.cpp

--- ascii.cpp
+++ ascii.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+using namespace std;
+
+int main (){
+char ch ;
+cout<<"Enter a character : ";
+cin>> ch ;
+/*A-Z = 65-90
+  a-z = 97-122
+  0-9 = 48-57*/
+cout << "ASCII value :" << int(ch) <<"\n";
+return 0;
+}
